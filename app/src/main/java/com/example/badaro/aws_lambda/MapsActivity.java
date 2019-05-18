c958ed78ba70b0b20528883d0955d85df845d93c
Rafael Badaro <rafael.badaro@mrv.com.br>
2019-05-18 12:55:36 -0300

    Lambda

--- app/src/main/java/com/example/badaro/aws_lambda/MapsActivity.java
+++ app/src/main/java/com/example/badaro/aws_lambda/MapsActivity.java
@@ -1,7 +1,17 @@
 package com.example.badaro.aws_lambda;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,11 +22,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.amazonaws.mobileconnectors.lambdainvoker.*;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.regions.Regions;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private LatLng pucs[];
+    private RequestClass request;
+    private MyInterface myInterface;
+    private LocationManager mLocationManager;
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,15 +42,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        configurarLambda();
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        getPermissions();
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500l,
+                100.0f, mLocationListener);
     }
 
-    private void criarArray(){
-            pucs[0] = new LatLng(-19.933209, -43.937179); // praça da liberdade
-            pucs[1] = new LatLng(-19.924365, -43.991588);// coreu
-            pucs[2] = new LatLng(-19.859570,-43.919052);// são gabriel
-            pucs[3] = new LatLng(-19.976627, -44.027777);// barreiro
-            pucs[4] = new LatLng(-19.954935, -44.198635);// betim
-            pucs[5] = new LatLng(-19.940546, -44.076264);// contagem
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            invocarLambda(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    public void getPermissions(){
+        if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }else{
+            return;
+        }
     }
 
     /**
@@ -48,13 +100,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        criarArray();
+        List<LatLng> pucs = new ArrayList<>();
+        pucs.add(new LatLng(-19.933209, -43.937179)); // praça da liberdade
+        pucs.add(new LatLng(-19.924365, -43.991588));// coreu
+        pucs.add(new LatLng(-19.859570,-43.919052)); // sao gabriel
+        pucs.add(new LatLng(-19.976627, -44.027777)); //barreiro
+        pucs.add(new LatLng(-19.954935, -44.198635));//betim
+        pucs.add(new LatLng(-19.940546, -44.076264));//contagem
 
-        // Add a marker in Sydney and move the camera
-        for (int i = 0; i <= pucs.length; i++){
-            mMap.addMarker(new MarkerOptions().position(pucs[i]).title("Uma unidade da PUC MINAS"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(pucs[i]));
+
+        for (LatLng campus: pucs) {
+            mMap.addMarker(new MarkerOptions().position(campus).title("Uma unidade da PUC MINAS"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(campus));
         }
+    }
 
+
+
+    public void configurarLambda(){
+        // Create an instance of CognitoCachingCredentialsProvider
+        CognitoCachingCredentialsProvider cognitoProvider = new CognitoCachingCredentialsProvider(
+                this.getApplicationContext(), "us-east-2:c6cf8466-cfa2-43d0-8fac-8115b51befb8", Regions.US_EAST_2);
+
+        // Create LambdaInvokerFactory, to be used to instantiate the Lambda proxy.
+        LambdaInvokerFactory factory = new LambdaInvokerFactory(this.getApplicationContext(),
+                Regions.US_EAST_2, cognitoProvider);
+
+        // Create the Lambda proxy object with a default Json data binder.
+        // You can provide your own data binder by implementing
+        // LambdaDataBinder.
+         myInterface = factory.build(MyInterface.class);
+
+         request = new RequestClass("John", "Doe");
+    }
+
+    public void invocarLambda(Location location){
+        // The Lambda function invocation results in a network call.
+        // Make sure it is not called from the main thread.
+        new AsyncTask<RequestClass, Void, ResponseClass>() {
+            @Override
+            protected ResponseClass doInBackground(RequestClass... params) {
+                // invoke "echo" method. In case it fails, it will throw a
+                // LambdaFunctionException.
+                try {
+                    return myInterface.AndroidBackendLambdaFunction(params[0]);
+                } catch (LambdaFunctionException lfe) {
+                    Log.e("Tag", "Failed to invoke echo", lfe);
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(ResponseClass result) {
+                if (result == null) {
+                    return;
+                }
+
+                // Do a toast
+                Toast.makeText(MapsActivity.this, result.getResponseString(), Toast.LENGTH_LONG).show();
+            }
+        }.execute(request);
     }
 }
+
---
+++ app/src/main/java/com/example/badaro/aws_lambda/MyInterface.java
@@ -0,0 +1,13 @@
+package com.example.badaro.aws_lambda;
+
+import com.amazonaws.mobileconnectors.lambdainvoker.LambdaFunction;
+public interface MyInterface {
+
+    /**
+     * Invoke the Lambda function "AndroidBackendLambdaFunction".
+     * The function name is the method name.
+     */
+    @LambdaFunction
+    ResponseClass AndroidBackendLambdaFunction(RequestClass request);
+
+}
